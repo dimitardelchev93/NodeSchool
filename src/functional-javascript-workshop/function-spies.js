2bff33e3f934cdf2e81a74b5573b5ba404b37ccc
Dimitar Delchev <dimitardelchev93@abv.bg>
2020-01-07 20:26:38 +0200

    functional-javascript-workshop function-spies

---
+++ src/functional-javascript-workshop/function-spies.js
@@ -0,0 +1,12 @@
+module.exports = (target, method) => {
+    var originalFunction = target[method];
+    var result = { count: 0 };
+
+    target[method] = function() {
+        result.count++;
+
+        return originalFunction.apply(this, arguments);
+    }
+
+    return result;
+};
