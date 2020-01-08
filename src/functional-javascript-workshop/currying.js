1410835bbfab5eac5d8d265ecf1e3ab2b885700c
Dimitar Delchev <dimitardelchev93@abv.bg>
2020-01-08 14:35:45 +0200

    functional-javascript-workshop currying

---
+++ src/functional-javascript-workshop/currying.js
@@ -0,0 +1,15 @@
+function curryN(fn, n) {
+    if (!n) {
+        n = fn.length;
+    }
+
+    return function curriedN(arg) {
+        if (n <= 1) {
+            return fn(arg);
+        }
+
+        return curryN(fn.bind(this, arg), n - 1);
+    }
+}
+
+module.exports = curryN;
