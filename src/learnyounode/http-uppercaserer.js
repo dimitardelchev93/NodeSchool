2d93ca5558804a7d61d3555c5e8d6bc8b849c215
Dimitar Delchev <dimitardelchev93@abv.bg>
2020-01-09 19:10:05 +0200

    learnyounode http-uppercaserer check for post

--- src/learnyounode/http-uppercaserer.js
+++ src/learnyounode/http-uppercaserer.js
@@ -3,13 +3,17 @@ const http = require('http');
 http.createServer((req, res) => {
     let body = [];
 
+    if (req.method !== 'POST') {
+        return res.end('send me a POST\n')
+    }
+
     req.on('error', (err) => {
-      console.error(err);
+        console.error(err);
     }).on('data', (chunk) => {
       body.push(chunk);
     }).on('end', () => {
-      body = Buffer.concat(body).toString();
+        body = Buffer.concat(body).toString();
 
-      res.end(body.toUpperCase());
+        res.end(body.toUpperCase());
     });
 }).listen(process.argv[2]);
