658980759d0f5f195e225f7d7b115289787b4fd4
Dimitar Delchev <dimitardelchev93@abv.bg>
2020-01-12 22:52:47 +0200

    elementary-electron save-pdf

--- src/elementary-electron/cat-annot/index.js
+++ src/elementary-electron/cat-annot/index.js
@@ -1,6 +1,28 @@
 var picture = require('cat-picture');
 var image = require('lightning-image-poly');
 var src = picture.src;
+var remote = require('electron').remote;
+var fs = require('fs');
 picture.remove();
 
-var viz = new image('#visualization', null, [src], {hullAlgorithm: 'convex'});
+var viz = new image('#visualization', null, [src], { hullAlgorithm: 'convex' });
+
+function save() {
+    remote.getCurrentWebContents().printToPDF({
+        portrait: true
+    }, function (err, data) {
+        fs.writeFile('annotation.pdf', data, function (err) {
+            if (err) {
+                alert('error generating pdf! ' + err.message);
+            } else {
+                alert('pdf saved!');
+            };
+        });
+    });
+}
+
+window.addEventListener('keydown', function (e) {
+    if (e.keyCode == 80) {
+        save()
+    };
+})
