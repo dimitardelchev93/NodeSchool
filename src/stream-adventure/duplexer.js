dc698317185fe6875bf9c449862d7a6fdc6cf93e
Dimitar Delchev <dimitardelchev93@abv.bg>
2020-01-13 20:10:26 +0200

    stream-adventure duplexer

---
+++ src/stream-adventure/duplexer.js
@@ -0,0 +1,8 @@
+const spawn = require('child_process').spawn;
+const duplexer = require('duplexer2');
+
+module.exports = (cmd, args) => {
+    const ps = spawn(cmd, args);
+
+    return duplexer(ps.stdin, ps.stdout);
+};
